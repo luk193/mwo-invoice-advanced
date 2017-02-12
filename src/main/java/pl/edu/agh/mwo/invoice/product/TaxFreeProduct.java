ba7aa66f4332073e00e394ca0e3885628eb86144
student10 <student10@S7N-PROGLANG-12.pcoip.ki.agh.edu.pl>
2017-02-12 10:33:58 +0100

    Product Test done

--- src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/TaxFreeProduct.java
@@ -5,5 +5,13 @@ import java.math.BigDecimal;
 public class TaxFreeProduct extends Product {
 	public TaxFreeProduct(String name, BigDecimal price) {
 		super(name, price, BigDecimal.ZERO);
+		if (price.compareTo(BigDecimal.ZERO) == -1) {
+			throw new IllegalArgumentException();
+		}
+		else if(name==""){
+			throw new IllegalArgumentException();
+			
+			
+		}
 	}
 }
